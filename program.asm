f151f210f6a9b21685090c930a86a0b52685a237
Steve Rich <steven.john.rich@gmail.com>
2018-11-09 10:37:04 +0000

    Create program.asm
    Exploring 8605 assembler

---
+++ program.asm
@@ -0,0 +1,19 @@
+;<Program title>
+
+jmp start
+
+;data
+
+;code
+start: nop
+
+
+	mvi d, 05    ; Put 5 in register d
+  mvi e, 05    ; Put 5 in register e
+  add d;
+  add e;
+	sta 0022; store contents of accum in memory
+
+
+
+hlt
