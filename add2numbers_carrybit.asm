3ade834f8bbd9e1f21c9019b9617093786d8ff14
Steve Rich <steven.john.rich@gmail.com>
2018-11-09 15:37:56 +0000

    Update add2numbers_carrybit.asm

--- add2numbers_carrybit.asm
+++ add2numbers_carrybit.asm
@@ -1,7 +1,11 @@
-mvi d, 255    ; Put FF in register d (note assembler expects denary numbers)
-mvi e, 255    ; Put FF in register e (note assembler expects denary numbers)
+;mvi d, 255    ; Put FF in register d (denary numbers)
+;mvi e, 255    ; Put FF in register e (denary numbers)
+mvi d, 0ffh    ; Put FF in register d (hex number)
+mvi e, 0ffh    ; Put FF in register e (hex number)
+
+
 add d;
 add e;
-sta 0022; store contents of accum in memory
+sta 0100h; store contents of accum in memory
 
 ; note in this example, the carry bit is set as FF+FF is too large to fit in the 8bit accumulator.
