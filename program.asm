80ec4075511326d61c89304990dcd5b639895c1b
Steve Rich <steven.john.rich@gmail.com>
2018-11-09 12:06:51 +0000

    Formatting changes

--- program.asm
+++ program.asm
@@ -8,11 +8,11 @@ jmp start
 start: nop
 
 
-	mvi d, 05    ; Put 5 in register d
-  mvi e, 05    ; Put 5 in register e
-  add d;
-  add e;
-	sta 0022; store contents of accum in memory
+mvi d, 05    ; Put 5 in register d
+mvi e, 05    ; Put 5 in register e
+add d;
+add e;
+sta 0022; store contents of accum in memory
 
 
 
